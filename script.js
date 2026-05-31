466b74eb5a877b1ea184f673cc34ba56c19ff9c4
naufalmaulana <naufalmau3@gmail.com>
2026-05-31 14:07:38 +0700

    Inisialisasi DOM dan Validasi Elemen

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const inputPohon = document.getElementById('inputPohon');
+    const hasilPupuk = document.getElementById('hasilPupuk');
+
+    if (inputPohon && hasilPupuk) {
+        
+    } else {
+        console.error("Elemen inputPohon atau hasilPupuk tidak ditemukan di HTML!");
+    }
+});
